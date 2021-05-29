c74fd596206047014c0144bb94aea39237256096
ChadMeng <84863549+ChadMeng@users.noreply.github.com>
2021-05-29 21:33:35 +0200

    Add files via upload

---
+++ cutlist.py
@@ -0,0 +1,12 @@
+def cutlist(listx,devidenum):
+  length = len(listx)
+  finallist = []
+  sum = 0
+  flist = []
+  for i in range(int(length/devidenum)+1):
+    if i <length/devidenum:
+      finallist.append([x for x in listx[:devidenum]])
+      listx = listx[devidenum:]
+    elif i == int(length/devidenum) and length%devidenum != 0:
+      finallist.append(listx[:])
+  return finallist
