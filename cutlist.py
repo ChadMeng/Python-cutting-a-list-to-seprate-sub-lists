1e46a1d4b8aa28b6a586d315ad4e91ba02eeff18
ChadMeng <84863549+ChadMeng@users.noreply.github.com>
2021-05-29 21:39:47 +0200

    Update cutlist.py

--- cutlist.py
+++ cutlist.py
@@ -2,7 +2,6 @@ def cutlist(listx,devidenum):
   length = len(listx)
   finallist = []
   sum = 0
-  flist = []
   for i in range(int(length/devidenum)+1):
     if i <length/devidenum:
       finallist.append([x for x in listx[:devidenum]])
